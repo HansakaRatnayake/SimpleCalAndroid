3b8ce3f142230eb0cd43a88bc4f6db0e103ea672
Hanze2002 <112684974+Hanze2002@users.noreply.github.com>
2022-09-02 16:46:10 +0530

    Refactord the code
    Created switch to decode what is the mathematical equation

--- app/src/main/java/com/example/simplecalandroid/MainActivity.java
+++ app/src/main/java/com/example/simplecalandroid/MainActivity.java
@@ -14,116 +14,41 @@ public class MainActivity extends AppCompatActivity {
     TextView txtAnswer;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         lblNum1bar = findViewById(R.id.lblNum1bar);
-         lblNum2bar = findViewById(R.id.lblNum2bar);
-         txtAnswer = findViewById(R.id.txtAnswer);
+        lblNum1bar = findViewById(R.id.lblNum1bar);
+        lblNum2bar = findViewById(R.id.lblNum2bar);
+        txtAnswer = findViewById(R.id.txtAnswer);
     }
 
 
+    public void calculate(View v) {
 
-
-
-
-
-    public void plus(View v){
-
+        String tag = v.getTag().toString();
+        txtAnswer.setText(tag);
 
         String Num1 = lblNum1bar.getText().toString();
         double x = Double.parseDouble(Num1);
 
-
         String Num2 = lblNum2bar.getText().toString();
         double y = Double.parseDouble(Num2);
 
-        double Answer = x+y;
-
-
-        txtAnswer.setText(""+ Answer);
-    }
-
-
-
-    public void minus(View v){
-
-        EditText lblNum1bar = findViewById(R.id.lblNum1bar);
-        String Num1 = lblNum1bar.getText().toString();
-        double x = Double.parseDouble(Num1);
-
-        EditText lblNum2bar = findViewById(R.id.lblNum2bar);
-        String Num2 = lblNum2bar.getText().toString();
-        double y = Double.parseDouble(Num2);
-
-        double Answer = x-y;
-
-        TextView txtAnswer = findViewById(R.id.txtAnswer);
-        txtAnswer.setText(""+ Answer);
-
-
-    }
-
-
-    public void multiple(View v){
-
-        EditText lblNum1bar = findViewById(R.id.lblNum1bar);
-        String Num1 = lblNum1bar.getText().toString();
-        double x = Double.parseDouble(Num1);
-
-        EditText lblNum2bar = findViewById(R.id.lblNum2bar);
-        String Num2 = lblNum2bar.getText().toString();
-        double y = Double.parseDouble(Num2);
-
-        double Answer = x*y;
-
-        TextView txtAnswer = findViewById(R.id.txtAnswer);
-        txtAnswer.setText(""+ Answer);
-
-
-    }
-
-
-
-    public void devide(View v){
-
-        EditText lblNum1bar = findViewById(R.id.lblNum1bar);
-        String Num1 = lblNum1bar.getText().toString();
-        double x = Double.parseDouble(Num1);
-
-        EditText lblNum2bar = findViewById(R.id.lblNum2bar);
-        String Num2 = lblNum2bar.getText().toString();
-        double y = Double.parseDouble(Num2);
-
-        double Answer = x/y;
-
-        TextView txtAnswer = findViewById(R.id.txtAnswer);
-        txtAnswer.setText(""+ Answer);
-
-
-    }
-
-
-
-    public void reset(View v){
-
-        double Num1 = 0;
-        double Num2 = 0;
         double Answer = 0;
 
-        EditText lblNum1bar = findViewById(R.id.lblNum1bar);
-        lblNum1bar.setText(""+ Num1);
+        switch (tag){
 
-        EditText lblNum2bar = findViewById(R.id.lblNum2bar);
-        lblNum2bar.setText(""+ Num2);
+            case "add": Answer = x + y; break;
+            case "minus": Answer = x - y; break;
+            case "multi": Answer = x * y; break;
+            case "devide": Answer = x / y; break;
 
-        TextView txtAnswer = findViewById(R.id.txtAnswer);
-        txtAnswer.setText(""+ Answer);
-
-
+        }
+        txtAnswer.setText("" + Answer);
     }
 }
+
+
+
